fec748363f2b0556217cd9928ea7ce1547ff3346
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2023-11-04 14:58:39 +0330

    score added

--- js/Game-Play.js
+++ js/Game-Play.js
@@ -1,13 +1,16 @@
 const container = document.querySelector(".container")
 const loader = document.getElementById("loader")
 const Question = document.querySelector(".question p")
-const Answers = document.querySelectorAll(".answer button")
+const Answers = document.querySelectorAll(".answer-text")
+const score = document.getElementById("score")
 
 const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
-
+const bonuz = 10
 let formatedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let scoreNumber = 0;
+let isAccepted = true;
 
 const formatData = (questionsData) => {
     const result = questionsData.map(item => {
@@ -33,11 +36,13 @@ const fetchData =async () => {
    console.log(formatedData)
    start()
 }
+
 const start = () => {
     showQuestion()
     container.style.display = "block" ;
     loader.style.display = "none"
 }
+
 const showQuestion = () => {
     const {question, answers ,indexCorrectAnswer} = formatedData[questionIndex]
     correctAnswer = indexCorrectAnswer
@@ -48,17 +53,26 @@ const showQuestion = () => {
 }
 
 const checkAnswer = (event,index) => {
+    if(!isAccepted) return;
+    isAccepted = false
+    
     const isCorrect = index === correctAnswer ? true : false;
     console.log(isCorrect)
     if(isCorrect){
-        event.target.classList.add("correct-answer")
+        console.log(event.target)
+        event.target.classList.add("correct-answer");
+        scoreNumber+=bonuz ;
+        score.innerText = scoreNumber;
     }
     else{
         event.target.classList.add("incorrect-answer");
         Answers[correctAnswer].classList.add("correct-answer");
     }
+    
 }
+
+window.addEventListener("load",fetchData)
+
 Answers.forEach((button,index) => {
     button.addEventListener("click",(event)=>checkAnswer(event, index))
 })
-window.addEventListener("load",fetchData)
